e4928ea4ab20640d956b43197f5abe54423a6fe0
mintesinotbizuayehw <mintesenotbizuayehw@gmail.com>
2025-05-19 15:10:01 +0300

    Your change description

--- ikimtech/src/component/services/Services.jsx
+++ ikimtech/src/component/services/Services.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import '../services/servise.css'
-function Services({logo,title,descrption}) {
+import './servise.css'
+
+function Services({ logo, title, descrption }) {
   return (
-    <div className="containers">
-      <div className="box left">
-        <div className="webcontainer">
-          <div className="weblogo">
-            <div className='weblogoimage'>
-              <img src={logo} alt="Service Logo" />
+    <div className="services-container">
+      <div className="service-box">
+        <div className="service-content">
+          <div className="service-header">
+            <div className='service-logo'>
+              <img src={logo} alt={`${title} Logo`} />
             </div>
-            <div className="webtitle">
+            <div className="service-title">
               <h2>{title}</h2>
             </div>
           </div>
-          <div className="descrption">
+          <div className="service-description">
             <p>{descrption}</p>
           </div>
         </div>
