977f8af9691622e3b70af6bfd987a0d09312b8a2
mintesinotbizuayehw <mintesenotbizuayehw@gmail.com>
2025-05-19 10:53:19 +0300

    paht

--- ikimtech/src/App.jsx
+++ ikimtech/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import About from "./component/about/About";
+import About from "./component/about/About";
 import Contact from "./component/Contact/Contact";
 import Header from "./component/header/Header";
 import Home from "./component/Home/Home";
@@ -18,7 +18,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomeContact />} /> {/* Default route */}
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} />
           <Route path="/Project" element={<Project />} />
           <Route path="/service" element={<Serviserapw />} />
           <Route path="/contact" element={<Contact />} />
--- ikimtech/src/component/HomeContact .jsx
+++ ikimtech/src/component/HomeContact .jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Home from './Home/Home'
-import Services from './services/Services'
+// import Services from './services/Services'
 import Project from './project/Project'
 import About from './about/About'
 import Contact from './Contact/Contact'
 import Serviserapw from './services/Serviserapw'
 
-function HomeContact () {
+function HomeContact() {
   return (
     <div>
       <Home/>
