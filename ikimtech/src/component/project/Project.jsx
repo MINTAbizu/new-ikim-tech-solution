128b4ce4795d1d2ec63b6e493e7e32e2c39b34b0
mintesinotbizuayehw <mintesenotbizuayehw@gmail.com>
2025-05-19 16:48:32 +0300

    Update Project.jsx
    on project page we hold our privous project

--- ikimtech/src/component/project/Project.jsx
+++ ikimtech/src/component/project/Project.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../project/project.css'; 
+import { Link } from 'react-router-dom';
 
 function Project({ image, title }) {
   return (
     <div className="projectcard">
-      <a href={image} className="media-1" data-fancybox="gallery">
+      <Link to={'https://www.ikimtechco.com/index.html#contact'} className="media-1" data-fancybox="gallery">
         <img src={image} alt={title} className="img-fluid" />
         <div className="media-1-content">
           <h2 className="h5">{title}</h2>
           <h2 className="h6">View</h2>
           <span className="category">Details</span>
         </div>
-      </a>
+      </Link>
     </div>
   );
 }
